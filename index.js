6df2a28ccd5560700773719d9d06a1ab70fd52ed
ribbinpo <ribbinpo@gmail.com>
2022-03-20 01:06:54 +0700

    v2.9

--- index.js
+++ index.js
@@ -138,33 +138,29 @@ const main = async (client) => {
 
 
 app.post('/callback',line.middleware(config),(req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
   main(client)
+  // Promise
+  //   .all(req.body.events.map(handleEvent))
+  //   .then((result) => res.json(result))
+  //   .catch((err) => {
+  //     console.error(err);
+  //     res.status(500).end();
+  //   });
 });
 
 // event handler
-function handleEvent(event) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-      // ignore non-text-message event
-      return Promise.resolve(null);
-    }
+// function handleEvent(event) {
+//     if (event.type !== 'message' || event.message.type !== 'text') {
+//       // ignore non-text-message event
+//       return Promise.resolve(null);
+//     }
   
-    // create a echoing text message
-    const echo = { type: 'text', text: event.message.text };
+//     // create a echoing text message
+//     const echo = { type: 'text', text: event.message.text };
   
-    // use reply API
-    return client.replyMessage(event.replyToken, echo);
-}
-  
-
-
-
+//     // use reply API
+//     return client.replyMessage(event.replyToken, echo);
+// }
 
 
 const PORT = process.env.PORT || 3000
