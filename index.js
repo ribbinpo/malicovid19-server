3c1ee1ed626755b3c491db61c9682a0262dbcdd3
ribbinpo <ribbinpo@gmail.com>
2022-03-20 01:54:27 +0700

    2.19

--- index.js
+++ index.js
@@ -139,30 +139,30 @@ const main = async (client) => {
 }
 
 
-// app.post('/callback',line.middleware(config),(req, res) => {
-//   Promise
-//     .all(req.body.events.map(handleEvent))
-//     .then((result) => res.json(result))
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).end();
-//     });
-// });
+app.post('/callback',line.middleware(config),(req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
 
 
 // event handler
-// function handleEvent(event) {
-//     if (event.type !== 'message' || event.message.type !== 'text') {
-//       // ignore non-text-message event
-//       return Promise.resolve(null);
-//     }
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      // ignore non-text-message event
+      return Promise.resolve(null);
+    }
   
-//     // create a echoing text message
-//     const echo = { type: 'text', text: event.message.text };
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
   
-//     // use reply API
-//     return client.replyMessage(event.replyToken, echo);
-// }
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+}
 
 main(client)
 
