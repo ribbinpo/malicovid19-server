65d9c3a3fa65ee4affb48e47aa430371149fde16
ribbinpo <ribbinpo@gmail.com>
2022-03-19 12:01:29 +0700

    v2.3 comment menu line

--- index.js
+++ index.js
@@ -33,111 +33,111 @@ app.get("/",(req,res)=>{
 // create LINE SDK client
 const client = new line.Client(config);
 
-const richMenuObjectA = () => ({
-  size: {
-    width: 2500,
-    height: 1686
-  },
-  selected: false,
-  name: "richmenu-a",
-  chatBarText: "Tap to open",
-  areas: [
-    {
-      bounds: {
-        x: 0,
-        y: 0,
-        width: 1250,
-        height: 1686
-      },
-      action: {
-        type: "uri",
-        uri: "https://www.line-community.me/"
-      }
-    },
-    {
-      bounds: {
-        x: 1251,
-        y: 0,
-        width: 1250,
-        height: 1686
-      },
-      action: {
-        type: "richmenuswitch",
-        richMenuAliasId: "richmenu-alias-b",
-        data: "richmenu-changed-to-b"
-      }
-    }
-  ]
-})
+// const richMenuObjectA = () => ({
+//   size: {
+//     width: 2500,
+//     height: 1686
+//   },
+//   selected: false,
+//   name: "richmenu-a",
+//   chatBarText: "Tap to open",
+//   areas: [
+//     {
+//       bounds: {
+//         x: 0,
+//         y: 0,
+//         width: 1250,
+//         height: 1686
+//       },
+//       action: {
+//         type: "uri",
+//         uri: "https://www.line-community.me/"
+//       }
+//     },
+//     {
+//       bounds: {
+//         x: 1251,
+//         y: 0,
+//         width: 1250,
+//         height: 1686
+//       },
+//       action: {
+//         type: "richmenuswitch",
+//         richMenuAliasId: "richmenu-alias-b",
+//         data: "richmenu-changed-to-b"
+//       }
+//     }
+//   ]
+// })
 
-const richMenuObjectB = () => ({
-  size: {
-    width: 2500,
-    height: 1686
-  },
-  selected: false,
-  name: "richmenu-b",
-  chatBarText: "Tap to open",
-  areas: [
-    {
-      bounds: {
-        x: 0,
-        y: 0,
-        width: 1250,
-        height: 1686
-      },
-      action: {
-        type: "richmenuswitch",
-        richMenuAliasId: "richmenu-alias-a",
-        data: "richmenu-changed-to-a"
-      }
-    },
-      {
-      bounds: {
-        x: 1251,
-        y: 0,
-        width: 1250,
-        height: 1686
-      },
-      action: {
-        type: "uri",
-        uri: "https://www.line-community.me/"
-      }
-    }
-  ]
-})
+// const richMenuObjectB = () => ({
+//   size: {
+//     width: 2500,
+//     height: 1686
+//   },
+//   selected: false,
+//   name: "richmenu-b",
+//   chatBarText: "Tap to open",
+//   areas: [
+//     {
+//       bounds: {
+//         x: 0,
+//         y: 0,
+//         width: 1250,
+//         height: 1686
+//       },
+//       action: {
+//         type: "richmenuswitch",
+//         richMenuAliasId: "richmenu-alias-a",
+//         data: "richmenu-changed-to-a"
+//       }
+//     },
+//       {
+//       bounds: {
+//         x: 1251,
+//         y: 0,
+//         width: 1250,
+//         height: 1686
+//       },
+//       action: {
+//         type: "uri",
+//         uri: "https://www.line-community.me/"
+//       }
+//     }
+//   ]
+// })
 
-const main = async (client) => {
-  // 2. Create rich menu A (richmenu-a)
-  const richMenuAId = await client.createRichMenu(
-    richMenuObjectA()
-  )
+// const main = async (client) => {
+//   // 2. Create rich menu A (richmenu-a)
+//   const richMenuAId = await client.createRichMenu(
+//     richMenuObjectA()
+//   )
 
-  // 3. Upload image to rich menu A
-  const filepathA = join(__dirname, './public/richmenu-a.png')
-  const bufferA = readFileSync(filepathA)
+//   // 3. Upload image to rich menu A
+//   const filepathA = join(__dirname, './public/richmenu-a.png')
+//   const bufferA = readFileSync(filepathA)
 
-  await client.setRichMenuImage(richMenuAId, bufferA)
+//   await client.setRichMenuImage(richMenuAId, bufferA)
 
-  // 4. Create rich menu B (richmenu-b)
-  const richMenuBId = await client.createRichMenu(richMenuObjectB())
+//   // 4. Create rich menu B (richmenu-b)
+//   const richMenuBId = await client.createRichMenu(richMenuObjectB())
 
-  // 5. Upload image to rich menu B
-  const filepathB = join(__dirname, './public/richmenu-b.png')
-  const bufferB = readFileSync(filepathB);
+//   // 5. Upload image to rich menu B
+//   const filepathB = join(__dirname, './public/richmenu-b.png')
+//   const bufferB = readFileSync(filepathB);
 
-  await client.setRichMenuImage(richMenuBId, bufferB);
+//   await client.setRichMenuImage(richMenuBId, bufferB);
 
-  // 6. Set rich menu A as the default rich menu
-  await client.setDefaultRichMenu(richMenuAId)
+//   // 6. Set rich menu A as the default rich menu
+//   await client.setDefaultRichMenu(richMenuAId)
 
-  // 7. Create rich menu alias A
-  await client.createRichMenuAlias(richMenuAId, 'richmenu-alias-a')
+//   // 7. Create rich menu alias A
+//   await client.createRichMenuAlias(richMenuAId, 'richmenu-alias-a')
 
-  // 8. Create rich menu alias B
-  await client.createRichMenuAlias(richMenuBId, 'richmenu-alias-b')
-  console.log('success')
-}
+//   // 8. Create rich menu alias B
+//   await client.createRichMenuAlias(richMenuBId, 'richmenu-alias-b')
+//   console.log('success')
+// }
 
 
 app.post('/callback', (req, res) => {
@@ -164,7 +164,7 @@ function handleEvent(event) {
     return client.replyMessage(event.replyToken, echo);
 }
 
-main(client)
+// main(client)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
