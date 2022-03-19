793b6e2a2d1693032509f3bdce23cc5fa2a649bd
ribbinpo <ribbinpo@gmail.com>
2022-03-19 12:30:07 +0700

    v2.6 test

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ const config = {
 // };
 
 //pre-defined middleware
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+// app.use(express.json())
+// app.use(express.urlencoded({
+//     extended: true
+// }))
 // app.use("/callback",line.middleware(config))
 // app.use(bodyParser.json())
 
@@ -140,31 +140,7 @@ const client = new line.Client(config);
 // }
 
 
-// app.post('/callback',line.middleware(config),(req, res) => {
-//     Promise
-//       .all(req.body.events.map(handleEvent))
-//       .then((result) => res.json(result))
-//       .catch((err) => {
-//         console.error(err);
-//         res.status(500).end();
-//       });
-// });
-
-// // event handler
-// function handleEvent(event) {
-//     if (event.type !== 'message' || event.message.type !== 'text') {
-//       // ignore non-text-message event
-//       return Promise.resolve(null);
-//     }
-  
-//     // create a echoing text message
-//     const echo = { type: 'text', text: event.message.text };
-  
-//     // use reply API
-//     return client.replyMessage(event.replyToken, echo);
-// }
-
-app.post('/callback', line.middleware(config), (req, res) => {
+app.post('/callback',line.middleware(config),(req, res) => {
     Promise
       .all(req.body.events.map(handleEvent))
       .then((result) => res.json(result))
@@ -172,103 +148,21 @@ app.post('/callback', line.middleware(config), (req, res) => {
         console.error(err);
         res.status(500).end();
       });
-  });
-  
-  // event handler
-  function handleEvent(event){
+});
+
+// event handler
+function handleEvent(event) {
     if (event.type !== 'message' || event.message.type !== 'text') {
       // ignore non-text-message event
       return Promise.resolve(null);
-    }else if(event.message.type==="text" && event.message.text==="UserID"){
-      const payload = {
-        type: "text",
-        text: event.source.userId
-      };
-      return client.replyMessage(event.replyToken,payload);
-    }else if(event.message.type==="text" && event.message.text.charAt(0)==='!'){
-      let command = event.message.text.split(" ")
-  
-      if(command[0]==='!connect'){
-        let sql = "UPDATE user SET ID_line = ? WHERE username=? AND password =?"
-        db.query(sql,[event.source.userId,command[1],command[2]],(err,result,field)=>{
-          if(err){ console.log("Internal Server Error to query upload status of history") }
-          else if(result.affectedRows>0){
-            const payload = {
-              type: "text",
-              text: "Line login success"
-            }
-            return client.replyMessage(event.replyToken,payload);
-          }else{
-            const payload = {
-              type: "text",
-              text: "Wrong username or password"
-            }
-          }
-        })
-      }else if(command[0]==="!disconnect"){
-        let sql = "UPDATE user SET ID_line = null WHERE ID_line = '"+event.source.userId+"'"
-        db.query(sql,(err,result,field)=>{
-          if(err){ console.log("Internal Server Error to query upload status of history") }
-          else if(result.affectedRows>0){
-            const payload = {
-              type: "text",
-              text: "disconnect success"
-            }
-            return client.replyMessage(event.replyToken,payload);
-          }else{
-            const payload = {
-              type: "text",
-              text: "disconnect Error"
-            }
-            return client.replyMessage(event.replyToken,payload);
-          }
-        })
-      }else if(command[0]==='!find'){
-        let sql = "SELECT name,status FROM book WHERE name LIKE '%"+command[1]+"' OR name LIKE '"+command[1]+"%' OR name LIKE '%"+command[1]+"%'"
-        db.query(sql,(err,result,field)=>{
-          if(err){ console.log("Internal Server Error to query upload status of history") }
-          else if(result.affectedRows>0){
-            const payload = {
-              type: "text",
-              text: result[0].name+" is "+result[0].status+" now."
-            }
-            return client.replyMessage(event.replyToken,payload);
-          }else{
-            const payload = {
-              type: "text",
-              text: "Not Found book"
-            }
-            return client.replyMessage(event.replyToken,payload);
-          }
-        })
-      }else if(command[0]==='!history'){
-        let sql = "SELECT user.name,history.due_date FROM history INNER JOIN user ON history.ID_user = user.ID_user INNER JOIN book ON history.ID_book=book.ID_book WHERE user.ID_line= ? AND history.Detail='Booking'"
-        db.query(sql,[event.source.userId],(err,result,field)=>{
-          if(err){ console.log("Internal Server Error to query upload status of history") }
-          else if(result.affectedRows>0){
-            let message = ""
-            for(let i=0;i<result.affectedRows;i++){
-              message = result[i].name+" due-date: "+result[i].due_date
-              let payload = {
-                type: "text",
-                text: message
-              }
-              client.replyMessage(event.replyToken,payload);
-            }
-            return ;
-          }else{
-            const payload = {
-              type: "text",
-              text: "Don't have history"
-            }
-            client.replyMessage(event.replyToken,payload);
-          }
-        })
-      }
-  
-      return client.replyMessage(event.replyToken,payload);
     }
-  }
+  
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+  
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+}
   
 
 
