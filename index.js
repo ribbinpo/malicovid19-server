8bda00143cf370609ce64b779edfa83c1b66f640
ribbinpo <ribbinpo@gmail.com>
2022-03-20 02:00:24 +0700

    2.21

--- index.js
+++ index.js
@@ -152,17 +152,17 @@ app.post('/callback',line.middleware(config),(req, res) => {
 
 // event handler
 function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }else if(event.type == 'message' || event.message.type == 'text'){
+        // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+  }
   main(client)
-    if (event.type !== 'message' || event.message.type !== 'text') {
-      // ignore non-text-message event
-      return Promise.resolve(null);
-    }
-  
-    // create a echoing text message
-    const echo = { type: 'text', text: event.message.text };
-  
-    // use reply API
-    return client.replyMessage(event.replyToken, echo);
     
 }
 
