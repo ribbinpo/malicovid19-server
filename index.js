ba27f92d9c009f85dadd9cee8f9734d9379fc268
ribbinpo <ribbinpo@gmail.com>
2022-03-27 04:26:12 +0700

    v4.2 Update card prediction on predict command

--- index.js
+++ index.js
@@ -103,13 +103,6 @@ const handleEvent = (event) => {
                     }
                   ]
                 },
-                "hero": {
-                  "type": "image",
-                  "url": "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
-                  "size": "full",
-                  "aspectRatio": "1.51:1",
-                  "aspectMode": "fit"
-                },
                 "body": {
                   "type": "box",
                   "layout": "vertical",
